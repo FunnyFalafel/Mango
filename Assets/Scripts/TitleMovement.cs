62e092784f0db22cb223f1b2a00a4de33e62dd66
mondaydavid <mondaydevid@gmail.com>
2026-01-31 10:34:25 -0500

    File Inserts

---
+++ Assets/Scripts/TitleMovement.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class TitleMovement : MonoBehaviour
+{
+    public float offsetMultiplier = 1f;
+    public float smoothTime = .3f;
+
+    private Vector2 startPosition;
+    private Vector3 velocity;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        Vector2 offset = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        transform.position = Vector3.Smoothamp(transform.position, startPosition * (offset * offsetMultiplier), ref velocity, smoothTime);
+
+    }
+}
