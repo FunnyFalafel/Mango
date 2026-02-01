0f131deae724ef2a8b897b9d4dad7bf719296130
mondaydavid <mondaydevid@gmail.com>
2026-02-01 14:15:37 -0500

    updated working

--- Assets/ScriptsTitle/MainMenuButtons.cs
+++ Assets/ScriptsTitle/MainMenuButtons.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 public class MainMenuButtons : MonoBehaviour
 {
     public void Play() {
-        LevelManager.Instance.LoadScene("GameplayScene", "CrossFade");
+        LevelManager.Instance.LoadScene("SampleScene_Brendan", "CrossFade");
     }
 
     public void Quit() {
